41616c165dbf7b728e44bf84d9dacc76d9c8567b
Kybar <kybar@protonmail.com>
2023-09-28 17:19:14 -0700

    Dot file updates

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 cd "$(dirname "$0")"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -6,11 +6,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 mkdir -p ~/.config/nvim
 
 rm ~/.tmux.conf
-rm ~/.zshrc
-rm ~/.config/nvim/init.vim
+rm ~/.config/nvim/init.lua
 
 
-ln -s ~/.dot/init.vim ~/.config/nvim/init.vim
+ln -s ~/.dot/init.lua ~/.config/nvim/init.lua
 ln -s ~/.dot/.tmux.conf ~/.tmux.conf
-ln -s ~/.dot/.zshrc ~/.zshrc
-nvim +PlugInstall
